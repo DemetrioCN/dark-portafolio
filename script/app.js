a97cd18b572be473807cedb44eefbde35f540a10
Demetrio <dem9411@gmail.com>
2021-04-16 04:47:49 -0500

    Create sticky header nav bar

--- script/app.js
+++ script/app.js
@@ -1,3 +1,9 @@
 function myFunction() {
   document.getElementById("myNav").classList.toggle("newNavCSS");
 }
+
+/* Navbar stcky */
+window.addEventListener("scroll", function () {
+  var nav = document.getElementById("nav_sticky");
+  nav.classList.toggle("sticky", window.scrollY > 0);
+})
