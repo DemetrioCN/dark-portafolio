b22b222c63daa45d78b0486f9f64f3566960fd7a
Demetrio <dem9411@gmail.com>
2021-04-18 00:18:24 -0500

    Comment codee

--- script/app.js
+++ script/app.js
@@ -1,8 +1,10 @@
+/* This function create a new class for to give another style for nav section */
 function myFunction() {
   document.getElementById("myNav").classList.toggle("newNavCSS");
 }
 
-/* Navbar stcky */
+
+/* Navbar stcky: Create a fixed navbar with diferent style*/
 window.addEventListener("scroll", function () {
   var nav = document.getElementById("nav_sticky");
   nav.classList.toggle("sticky", window.scrollY > 0);
